e4fcb5b463ee6738e4d54356ffdcd92e42020fb8
Gabriel Ajenifuja <gabrielfuja001@gmail.com>
2024-10-01 20:54:30 +0100

    Structured data extraction

--- src/app/api/process/route.ts
+++ src/app/api/process/route.ts
@@ -1,11 +1,39 @@
 import OpenAI from 'openai'
 import { NextRequest, NextResponse } from 'next/server';
-
+import { z } from "zod";
+import { zodResponseFormat } from "openai/helpers/zod";
 const openai = new OpenAI({
     apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 })
+
+const transactionExtraction = z.object({
+    account_holder: z.object({
+        name: z.string(),
+        address: z.string(),
+        phone_number: z.string(),
+        email: z.string(),
+    }),
+    statement_period: z.object({
+        start_date: z.string(),
+        end_date: z.string(),
+    }),
+    balances: z.object({
+        opening_balance: z.string(),
+        closing_balance: z.string(),
+        money_out: z.string(),
+        money_in: z.string(),
+    }),
+    transactions: z.array(z.object({
+        date: z.string(),
+        description: z.string(),
+        debit: z.string(),
+        credit: z.string(),
+        balance: z.string(),
+    })),
+});
+
 export async function POST(request: NextRequest) {
-    let responseContent = '';
+    let responseContent = {};
     const transactions = [];
     try {
         const { contents } = await request.json()
@@ -14,76 +42,28 @@ export async function POST(request: NextRequest) {
         for (const content of contents) {
             // Process the data using OpenAI
             const prompt = `You are an expert in parsing bank statements.
-            Given the following text from a bank statement, extract and structure the data properly into a JSON format.
+            You will be given unstructured text from a bank statement, extract and structure the data properly into a JSON format.
             Include fields like date, description, debit, credit and balance.
             The date should be in the format of d/m/Y hh:mm.
             Text: ${content}.
 
-            The output should be in the following JSON format only, validate that output is a valid javascript JSON format to be parsed {} remove unnecessary text such as "Based on the provided bank statement, here's the extracted data in a structured JSON format:" and backtick characters: \n\n
-            {
-            "account_holder": {
-                "name": "",
-                "address": "",
-                "phone_number": "",
-                "email": ""
-            },
-            "statement_period": {
-                "start_date": "",
-                "end_date": ""
-            },
-            "balances": {
-                "opening_balance": "",
-                "closing_balance": "",
-                "money_out": "",
-                "money_in": "",
-            },
-            "transactions": [
-                {
-                    "date": "",
-                    "description": "",
-                    "debit": "",
-                    "credit": "",
-                    "balance": ""
-                }
-            ]
-        }`;
+            The output should be in the following JSON format only, validate that output is a valid javascript JSON format to be parsed {} remove unnecessary text, backtick and characters`;
 
-            const completion = await openai.chat.completions.create({
-                model: "gpt-3.5-turbo-1106",
-                messages: [{ role: "user", content: prompt }],
+            const completion = await openai.beta.chat.completions.parse({
+                model: "gpt-4o-mini",
+                messages: [{ role: "system", content: prompt }],
+                response_format:  zodResponseFormat(transactionExtraction, "transactionExtraction"),
             })
 
-            responseContent = completion.choices[0].message.content || ''
-
-            //console.log(responseContent)
-            // Clean up the response content
-            const chars = ['```sql', '```', '```json', 'json'];
-            chars.forEach(char => {
-                responseContent = responseContent.replace(char, '');
-            });
-
-            responseContent = responseContent.replace(/```(json|sql)?|json/g, '');
-            //console.log(responseContent)
-            // Attempt to parse the JSON, with error handling
-            let processedData;
-            try {
-                processedData = JSON.parse(responseContent.trim());
-            } catch (parseError) {
-                console.error('Error parsing JSON:', parseError);
-                // If parsing fails, attempt to format the data
-                processedData = formatData(responseContent);
+            responseContent = completion.choices[0].message?.parsed || {}
+         
+            if ('transactions' in responseContent) {
+                transactions.push(responseContent.transactions);
             }
-
-            if (processedData && processedData.transactions) {
-                transactions.push(processedData.transactions);
-            } else {
-                console.warn('No transactions found in processed data');
-            }
+            
         }
 
         const mergedTransactions = transactions.flat();
-
-        //console.log(mergedTransactions);
 
         return NextResponse.json({ transactions: mergedTransactions }, { status: 200 });
     } catch (error) {
