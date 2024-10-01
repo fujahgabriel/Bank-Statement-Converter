31ee2e1cee7058f849330cb7b420fbc18e6894ef
Gabriel Ajenifuja <gabrielfuja001@gmail.com>
2024-10-01 11:59:23 +0100

    added analytics

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-      OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+      NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
     },
   }
   
